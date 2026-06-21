b6b2942e247c83e07ed794e8cdaa7be638972cdf
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 18:42:53 -0300

    feat: create user repository

---
+++ src/modules/users/user.repository.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/config/prisma";
+import { createUserDTO } from "./user.type";
+
+export const userRepository = {
+  async findById(id: string) {
+    return prisma.user.findUnique({
+      where: {
+        id,
+      }
+    })
+  },
+
+  async findByEmail(email: string) {
+    return prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+  },
+
+  async create(data: createUserDTO) {
+    return prisma.user.create({
+      data,
+    })
+  },
+}
