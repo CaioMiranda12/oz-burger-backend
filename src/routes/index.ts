130d8d30331aecac64ca7e0244cb28af9e89a14a
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 15:19:23 -0300

    feat: add product routes on routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
+import { productRoutes } from "@/modules/products/product.routes";
 import { Request, Response, Router } from "express";
 
 const routes = Router();
 
 routes.get('/', (req: Request, res: Response) => {
-  return res.json('Hello World!');
+  return res.json({ message: 'Hello World!' });
 })
+
+routes.use('/products', productRoutes);
 
 export { routes };
