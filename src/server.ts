e0738563a56669feac3c75932afa2b4881fc4bc9
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 12:05:33 -0300

    feat: add server port on .env

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import 'dotenv/config';
 import { app } from './app';
 
-app.listen(3000, () => console.log('Server is running at port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
