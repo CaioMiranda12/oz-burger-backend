f4c9699acace7586871008b9bed5df31b0277517
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 21:28:26 -0300

    feat: add authenticated user type on jwt utils

--- src/shared/utils/jwt.ts
+++ src/shared/utils/jwt.ts
@@ -1,11 +1,15 @@
+import { AuthenticatedUser } from '@/modules/users/user.type';
 import jwt from 'jsonwebtoken'
 
+const JWT_SECRET = process.env.JWT_SECRET as string;
+const EXPIRES_IN = '1d';
+
 export const jwtUtils = {
-  sign(payload: { id: string, role: string }) {
-    return jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1d' });
+  sign(payload: AuthenticatedUser) {
+    return jwt.sign(payload, JWT_SECRET as string, { expiresIn: EXPIRES_IN });
   },
 
-  verify(token: string) {
-    return jwt.verify(token, process.env.JWT_SECRET as string);
+  verify(token: string): AuthenticatedUser {
+    return jwt.verify(token, JWT_SECRET) as AuthenticatedUser;
   },
 }
