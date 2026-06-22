74379e98ade967cbb8f09563b0a57711456b2fbe
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 23:50:31 -0300

    feat: add category routes on routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { authRoutes } from "@/modules/auth/auth.routes";
+import { categoryRoutes } from "@/modules/categories/category.routes";
 import { productRoutes } from "@/modules/products/product.routes";
 import { Request, Response, Router } from "express";
 
@@ -10,5 +11,6 @@ routes.get('/', (req: Request, res: Response) => {
 
 routes.use('/products', productRoutes);
 routes.use('/auth', authRoutes);
+routes.use('/categories', categoryRoutes);
 
 export { routes };
