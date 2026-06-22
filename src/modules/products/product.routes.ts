9a5ac410ff5847ca27826e4105e936298f52a14c
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 21:45:43 -0300

    feat: add ensure role middleware on admin routes of products routes

--- src/modules/products/product.routes.ts
+++ src/modules/products/product.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { productController } from "./product.controller";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { ensureRole } from "@/middlewares/ensureRole";
 
 
 const productRoutes = Router();
@@ -10,7 +11,7 @@ productRoutes.get('/:id', productController.findProductById);
 
 const adminRoutes = Router();
 // adicionar futuramente middleware de admin
-adminRoutes.use(ensureAuthenticated);
+adminRoutes.use(ensureAuthenticated, ensureRole("ADMIN"));
 adminRoutes.post('/', productController.createProduct);
 adminRoutes.put('/:id', productController.updateProduct);
 adminRoutes.delete('/:id', productController.deleteProduct);
