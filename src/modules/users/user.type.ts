ce19803a3790859dfe6a0899a194aca6211f626c
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 21:28:37 -0300

    feat: create authenticated user interface

--- src/modules/users/user.type.ts
+++ src/modules/users/user.type.ts
@@ -1,5 +1,11 @@
+import { Role } from "@/generated/prisma/client";
 export interface createUserDTO {
   name: string;
   email: string;
   password: string;
+}
+
+export interface AuthenticatedUser {
+  id: string;
+  role: Role;
 }
