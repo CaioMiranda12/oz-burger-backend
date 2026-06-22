a005f0b241685e00268e1412d0ea5fb2e2f8cdd2
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 21:54:28 -0300

    fix: fix prisma seed + add script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,19 @@
 import { PrismaClient } from '../src/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import bcrypt from 'bcrypt';
+import { AppError } from '../src/shared/errors/AppError'
 import 'dotenv/config';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  const ADMIN_EMAIL = process.env.ADMIN_EMAIL as string;
-  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD as string;
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
+  if (!ADMIN_EMAIL || !ADMIN_PASSWORD) {
+    throw new AppError('ADMIN_EMAIL and ADMIN_PASSWORD must be in .env');
+  }
 
   const hashedPassword = await bcrypt.hash(ADMIN_PASSWORD, 10);
 
