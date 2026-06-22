a31c52a7983a1c82ecb3e694abb0be4fa10b6573
CaioMiranda12 <caioclm04@gmail.com>
2026-06-21 22:14:31 -0300

    feat: add category id on create product schema

--- src/modules/products/product.schema.ts
+++ src/modules/products/product.schema.ts
@@ -4,6 +4,7 @@ export const createProductSchema = z.object({
   name: z.string().min(2),
   price: z.number().positive(),
   description: z.string().min(2),
+  categoryId: z.string(),
 })
 
 export const updateProductSchema = createProductSchema.partial().extend({
